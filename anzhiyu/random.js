218316658eaba1eee427f33053acde6afff8fad7
RiYueRY <3968773701@qq.com>
2025-05-02 14:34:44 +0000

    Site updated: 2025-05-02 14:34:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/01/从0开始学java/"];function toRandomPost(){
+var posts=["2025/05/02/Fate系列观看指南-从入门到入土/","2025/05/01/从0开始学java/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
