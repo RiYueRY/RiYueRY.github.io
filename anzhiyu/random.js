065f01c365248cb7c714ec8350069dfbbbf18790
RiYueRY <3968773701@qq.com>
2025-05-25 05:58:03 +0000

    Site updated: 2025-05-25 05:58:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/24/从入坑到精通的理性分析/"];function toRandomPost(){
+var posts=["2025/05/24/从入坑到精通的理性分析/","2025/05/24/从0开始学java/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
